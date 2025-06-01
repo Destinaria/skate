d8f4c5be85b53c6cca829450b35b824d4caaf854
Rosa Faria <jairsogaraio@gmail.com>
2025-06-01 01:19:57 -0300

    feat: add example

---
+++ example/script.js
@@ -0,0 +1,6 @@
+document.getElementById("inc").addEventListener("click", function() {
+  document.getElementById("count").innerHTML++;
+});
+document.getElementById("dec").addEventListener("click", function() {
+  document.getElementById("count").innerHTML--;
+});
